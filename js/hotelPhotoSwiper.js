6a620d28fcddc2f51564c974d5bac1ee86b417fc
Ayu Huang <star82620@gmail.com>
2023-07-18 15:39:27 +0800

    add hotel photo swiper

---
+++ js/hotelPhotoSwiper.js
@@ -0,0 +1,9 @@
+const hotelPhotoSwiper = new Swiper(".hotelPhotoSwiper", {
+  slidesPerView: 1,
+  allowTouchMove: false,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
