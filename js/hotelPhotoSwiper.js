4a9affa26fa0e5e61ee317455e384bd14534a76f
Ayu Huang <star82620@gmail.com>
2023-07-18 16:37:09 +0800

    adjust swiper effect

--- js/hotelPhotoSwiper.js
+++ js/hotelPhotoSwiper.js
@@ -3,7 +3,7 @@
 const hotelPhotoSwiper = new Swiper(".hotelPhotoSwiper", {
   slidesPerView: 1,
   allowTouchMove: false,
-
+  effect: "fade",
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
