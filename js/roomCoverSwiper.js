0abbf1f61bcf1ba778a1c834b7241be1d600502c
Ayu Huang <star82620@gmail.com>
2023-07-20 16:37:22 +0800

    finish feature of swiper photo zoom in

--- js/roomCoverSwiper.js
+++ js/roomCoverSwiper.js
@@ -3,20 +3,25 @@ const roomCoverContainer = document.querySelector(".roomCoverContainer");
 const roomCoverSwiper = document.querySelector(".roomCoverSwiper");
 const roomSwiperWrapper = roomCoverSwiper.querySelector(".swiper-wrapper");
 const photoZoomIn = document.querySelector(".photoZoomIn");
+let roomImages = "";
+let roomName = "";
+let imgIndex = 0;
+let isInMin = "";
+let isInMax = "";
+let prevState = "";
+let nextState = "";
 
 //Swiper 背景動態
 function renderRoomCover() {
-  const roomImages = roomDes.imageUrl;
-  const roomName = roomDes.name;
-  let imgIndex = 0;
+  roomImages = roomDes.imageUrl;
+  roomName = roomDes.name;
 
   // 生成 swiper 元素
   let roomSwiperHtml = "";
-  roomImages.forEach((img, index) => {
+  roomImages.forEach((img) => {
     const roomSwiperElement = `
       <div
-        class="swiper-slide"
-        data-imgindex="${index}"
+        class="swiper-slide""
         style="background-image:url('${img}');">
       </div>
     `;
@@ -36,15 +41,17 @@ function renderRoomCover() {
     },
   });
 
+  //切換幻燈片時就修改 imgIndex
+  roomSwiper.on("slideChange", function () {
+    imgIndex = roomSwiper.activeIndex;
+  });
+
   //點照片放大
-  roomCoverContainer.addEventListener("click", popupZoomIn);
+  roomCoverContainer.addEventListener("click", openZoomIn);
 }
 
-//popupZoomIn 生成 popup
-function popupZoomIn(e) {
-  const activeSlide = document.querySelector(".swiper-slide-active");
-  const activeSlideImg = activeSlide.style.backgroundImage.split('"')[1];
-  const activeSlideIndex = activeSlide.dataset.imgindex;
+//openZoomIn 生成 popup
+function openZoomIn(e) {
   const isBackRoomList = e.target.classList.contains("backRoomList");
   const isPagination = e.target.classList.contains("swiper-pagination-bullet");
   const isBookingButton = e.target.classList.contains("bookingButton");
@@ -54,23 +61,24 @@ function popupZoomIn(e) {
     return;
   }
 
-  // popup 出現
+  checkCoverRange();
+
+  prevState = isInMin ? "" : "disable";
+  nextState = isInMax ? "" : "disable";
+
+  //生成 popup HTML
   const zoomPhotoElement = `
         <div class="zoomBackdrop">
-          <div class="zoomPrevArrow">
-            <img src="./img/room/icon-arrow-left-white.svg" alt="icon-arrow-left-white" />
+            <img class="zoomPrevArrow ${prevState}" src="./img/room/icon-arrow-left-white.svg" alt="icon-arrow-left-white" />
+          <div> 
+            <img class="zoomPhoto" src="${roomImages[imgIndex]}" />
           </div>
-          <div class="zoomPhoto"> 
-            <img data-imgindex="${activeSlideIndex}" src="${activeSlideImg}" />
-          </div>
-          <div class="zoomNextArrow">
-            <img src="./img/room/icon-arrow-right-white.svg" alt="icon-arrow-right-white" />
-          </div>
+            <img class="zoomNextArrow ${nextState}" src="./img/room/icon-arrow-right-white.svg" alt="icon-arrow-right-white" />
         </div>
         `;
   photoZoomIn.innerHTML = zoomPhotoElement;
 
-  //關閉 popup: esc、點擊空白處
+  //點擊 popup
   window.addEventListener("keydown", (keydownEvent) => {
     if (keydownEvent.key === "Escape") {
       closePopup(keydownEvent);
@@ -80,13 +88,45 @@ function popupZoomIn(e) {
   photoZoomIn.addEventListener("click", closePopup);
 }
 
-//關閉 popup
+//檢查目前狀態
+function checkCoverRange() {
+  isInMin = imgIndex > 0;
+  isInMax = imgIndex < roomImages.length - 1;
+}
+
+//點擊 popup
 function closePopup(e) {
-  console.log("close!");
-
+  const zoomPhoto = document.querySelector(".zoomPhoto");
+  const zoomPrevArrow = document.querySelector(".zoomPrevArrow");
+  const zoomNextArrow = document.querySelector(".zoomNextArrow");
   const isPopupBackdrop = e.target.classList.contains("zoomBackdrop");
+  const isZoomPrev = e.target.classList.contains("zoomPrevArrow");
+  const isZoomNext = e.target.classList.contains("zoomNextArrow");
 
+  //關閉 popup
   if (isPopupBackdrop || e.key === "Escape") {
     photoZoomIn.innerHTML = "";
+  }
+
+  const prevIndex = imgIndex - 1;
+  const nextIndex = imgIndex + 1;
+
+  //可以按 Prev
+  if (isZoomPrev && isInMin) {
+    console.log("點擊了 Prev");
+    zoomPhoto.setAttribute("src", `${roomImages[prevIndex]}`);
+    imgIndex = prevIndex;
+    checkCoverRange();
+    zoomPrevArrow.classList.toggle("disable", !isInMin);
+    zoomNextArrow.classList.toggle("disable", !isInMax);
+  }
+
+  // 可以按 Next
+  if (isZoomNext && isInMax) {
+    zoomPhoto.setAttribute("src", `${roomImages[nextIndex]}`);
+    imgIndex = nextIndex;
+    checkCoverRange();
+    zoomPrevArrow.classList.toggle("disable", !isInMin);
+    zoomNextArrow.classList.toggle("disable", !isInMax);
   }
 }
